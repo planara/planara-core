bdd30ad512a14ad44184fe37c8623697d3be7de5
a1unade <tags0956@gmail.com>
2025-09-30 21:01:13 +0300

    [add] deploy-pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-import { Renderer } from './core/renderer';
-
-const canvas = document.getElementById('app') as HTMLCanvasElement;
-const renderer = new Renderer(canvas);
-renderer.loop();
-window.addEventListener('resize', renderer.resize, false);
-renderer.resize();
+export * from './core/renderer';
