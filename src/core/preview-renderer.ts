cb361c9c9460be616a27d753f511592ecfb79679
a1unade <tags0956@gmail.com>
2025-10-03 21:04:08 +0300

    [fix] linter-issue

--- src/core/preview-renderer.ts
+++ src/core/preview-renderer.ts
@@ -4,7 +4,7 @@ import { Renderer } from './renderer';
  * Рендерер для предпросмотра модели (сцена, камера, orbit по горизонтали)
  */
 export class PreviewRenderer extends Renderer {
-    protected init() {
-        // TODO: после загрузки модели добавить орбит только по горизонтали
-    }
+  protected init() {
+    // TODO: после загрузки модели добавить орбит только по горизонтали
+  }
 }
