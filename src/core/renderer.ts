c313b76c5d9576cb3737c98f740745e256e5a491
a1unade <tags0956@gmail.com>
2025-10-03 17:43:40 +0300

    [add] editor-renderer

--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -1,50 +1,42 @@
-import { Renderer as OGLRenderer, Camera, Transform, GridHelper, Orbit, AxesHelper } from 'ogl';
+import { Renderer as OGLRenderer, Camera, Transform } from 'ogl';
 
-export class Renderer {
-  public gl: OGLRenderer;
-  public scene: Transform;
-  public camera: Camera;
-  public orbit: Orbit;
-  private canvas: HTMLCanvasElement;
+export abstract class Renderer {
+    public gl: OGLRenderer;
+    public scene: Transform;
+    public camera: Camera;
+    protected canvas: HTMLCanvasElement;
 
-  constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas;
-    this.gl = new OGLRenderer({ canvas });
-    this.gl.setSize(canvas.clientWidth, canvas.clientHeight);
-    this.gl.gl.clearColor(1, 1, 1, 1);
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.gl = new OGLRenderer({ canvas });
+        this.gl.setSize(canvas.clientWidth, canvas.clientHeight);
+        this.gl.gl.clearColor(1, 1, 1, 1);
 
-    this.scene = new Transform();
+        this.scene = new Transform();
 
-    // Камера
-    this.camera = new Camera(this.gl.gl, { fov: 45 });
-    this.camera.position.set(1, 1, 7);
-    this.camera.lookAt([0, 0, 0]);
+        this.camera = new Camera(this.gl.gl, { fov: 45 });
+        this.camera.position.set(1, 1, 7);
+        this.camera.lookAt([0, 0, 0]);
 
-    // Сетка
-    const grid = new GridHelper(this.gl.gl, { size: 10, divisions: 10 });
-    grid.position.y = -0.001;
-    grid.setParent(this.scene);
+        this.init();
+    }
 
-    // Управление
-    this.orbit = new Orbit(this.camera);
+    protected abstract init(): void;
 
-    // Оси X, Y, Z
-    const axes = new AxesHelper(this.gl.gl, { size: 6, symmetric: true });
-    axes.setParent(this.scene);
-  }
+    resize() {
+        this.gl.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+        this.camera.perspective({ aspect: this.canvas.width / this.canvas.height });
+    }
 
-  resize() {
-    this.gl.setSize(window.innerWidth, window.innerHeight);
-    this.camera.perspective({ aspect: this.canvas.width / this.canvas.height });
-  }
+    render() {
+        this.gl.render({ scene: this.scene, camera: this.camera });
+    }
 
-  render() {
-    this.gl.render({ scene: this.scene, camera: this.camera });
-  }
+    loop() {
+        this.update();
+        this.render();
+        requestAnimationFrame(this.loop.bind(this));
+    }
 
-  loop() {
-    this.orbit.update();
-    this.render();
-    requestAnimationFrame(this.loop.bind(this));
-  }
+    protected update(): void {}
 }
