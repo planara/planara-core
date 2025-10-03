ff414d00cff351fb259f9c9f676076c77731ae12
a1unade <tags0956@gmail.com>
2025-10-03 17:49:38 +0300

    [fix] linter

--- src/core/editor-renderer.ts
+++ src/core/editor-renderer.ts
@@ -2,23 +2,23 @@ import { Renderer } from './renderer';
 import { GridHelper, Orbit, AxesHelper } from 'ogl';
 
 export class EditorRenderer extends Renderer {
-    private orbit!: Orbit;
+  private orbit!: Orbit;
 
-    protected init() {
-        // сетка
-        const grid = new GridHelper(this.gl.gl, { size: 10, divisions: 10 });
-        grid.position.y = -0.001;
-        grid.setParent(this.scene);
+  protected init() {
+    // сетка
+    const grid = new GridHelper(this.gl.gl, { size: 10, divisions: 10 });
+    grid.position.y = -0.001;
+    grid.setParent(this.scene);
 
-        // оси
-        const axes = new AxesHelper(this.gl.gl, { size: 6, symmetric: true });
-        axes.setParent(this.scene);
+    // оси
+    const axes = new AxesHelper(this.gl.gl, { size: 6, symmetric: true });
+    axes.setParent(this.scene);
 
-        // orbit
-        this.orbit = new Orbit(this.camera);
-    }
+    // orbit
+    this.orbit = new Orbit(this.camera);
+  }
 
-    protected update() {
-        this.orbit.update();
-    }
+  protected update() {
+    this.orbit.update();
+  }
 }
--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -1,42 +1,42 @@
 import { Renderer as OGLRenderer, Camera, Transform } from 'ogl';
 
 export abstract class Renderer {
-    public gl: OGLRenderer;
-    public scene: Transform;
-    public camera: Camera;
-    protected canvas: HTMLCanvasElement;
+  public gl: OGLRenderer;
+  public scene: Transform;
+  public camera: Camera;
+  protected canvas: HTMLCanvasElement;
 
-    constructor(canvas: HTMLCanvasElement) {
-        this.canvas = canvas;
-        this.gl = new OGLRenderer({ canvas });
-        this.gl.setSize(canvas.clientWidth, canvas.clientHeight);
-        this.gl.gl.clearColor(1, 1, 1, 1);
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.gl = new OGLRenderer({ canvas });
+    this.gl.setSize(canvas.clientWidth, canvas.clientHeight);
+    this.gl.gl.clearColor(1, 1, 1, 1);
 
-        this.scene = new Transform();
+    this.scene = new Transform();
 
-        this.camera = new Camera(this.gl.gl, { fov: 45 });
-        this.camera.position.set(1, 1, 7);
-        this.camera.lookAt([0, 0, 0]);
+    this.camera = new Camera(this.gl.gl, { fov: 45 });
+    this.camera.position.set(1, 1, 7);
+    this.camera.lookAt([0, 0, 0]);
 
-        this.init();
-    }
+    this.init();
+  }
 
-    protected abstract init(): void;
+  protected abstract init(): void;
 
-    resize() {
-        this.gl.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
-        this.camera.perspective({ aspect: this.canvas.width / this.canvas.height });
-    }
+  resize() {
+    this.gl.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+    this.camera.perspective({ aspect: this.canvas.width / this.canvas.height });
+  }
 
-    render() {
-        this.gl.render({ scene: this.scene, camera: this.camera });
-    }
+  render() {
+    this.gl.render({ scene: this.scene, camera: this.camera });
+  }
 
-    loop() {
-        this.update();
-        this.render();
-        requestAnimationFrame(this.loop.bind(this));
-    }
+  loop() {
+    this.update();
+    this.render();
+    requestAnimationFrame(this.loop.bind(this));
+  }
 
-    protected update(): void {}
+  protected update(): void {}
 }
