e1116746655086dda84ffba78693dd4b934fd471
a1unade <tags0956@gmail.com>
2026-04-10 13:06:44 +0300

    [add] minify package files for build

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     }),
   ],
   build: {
-    minify: false,
+    minify: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'PlanaraCore',
